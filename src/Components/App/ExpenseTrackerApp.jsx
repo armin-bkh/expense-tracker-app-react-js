04ec95f65877dc5c3988400db74ef917ca68fe5b
armin-bkh <arminbkh0921@yahoo.com>
2022-01-06 13:47:35 +0330

    change ui

--- src/Components/App/ExpenseTrackerApp.jsx
+++ src/Components/App/ExpenseTrackerApp.jsx
@@ -58,7 +58,7 @@ const ExpenseTrackerApp = () => {
   return (
     <section
       className={
-        "bg-violet-400 dark:bg-violet-600 dark:shadow-violet-600/90 p-3 md:p-5 transition-all rounded-xl shadow-2xl shadow-violet-400/90 w-full lg:w-10/12 mx-auto"
+        "bg-violet-400 dark:bg-violet-600 dark:shadow-violet-600/90 p-3 md:p-5 transition-all rounded-xl shadow-2xl shadow-violet-400/90 w-full mx-auto"
       }
     >
       <OverView
