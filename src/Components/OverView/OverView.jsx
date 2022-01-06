d45691d4ae666433e4ff12d86fd29b6d30efcc5d
armin-bkh <arminbkh0921@yahoo.com>
2022-01-06 09:14:11 +0330

    change ui TransacitonForm component

--- src/Components/OverView/OverView.jsx
+++ src/Components/OverView/OverView.jsx
@@ -10,7 +10,7 @@ const OverView = ({ expense, income, addTransactions }) => {
       <article className={styles.balance}>
         <span className="text-violet-200">Balance: {income - expense}</span>
         <button
-          className={`px-7 py-2 rounded-md font-bold ${
+          className={`px-7 py-2 rounded-md font-bold transition duration-500 ${
             !isShow
               ? "bg-violet-200 text-violet-600 shadow-md shadow-violet-200/50"
               : "bg-violet-600 text-violet-200 shadow-md shadow-violet-600/50"
@@ -26,12 +26,12 @@ const OverView = ({ expense, income, addTransactions }) => {
           setIsShow={setIsShow}
         />
       )}
-      <article className={`flex justify-around items-center`}>
-        <div className="flex items-center border-2 border-violet-200 py-14 px-20 rounded-lg shadow-lg shadow-violet-200/70">
+      <article className={`flex justify-around items-center text-violet-600`}>
+        <div className="flex items-center py-14 px-20 rounded-lg shadow-lg shadow-violet-200/70 bg-violet-200">
           Expense
           <span className="text-red-600 ml-3">{expense} $</span>
         </div>
-        <div className="flex items-center border-2 border-violet-200 py-14 px-20 rounded-lg shadow-lg shadow-violet-200/70">
+        <div className="flex items-center py-14 px-20 rounded-lg shadow-lg shadow-violet-200/70 bg-violet-200">
           Income
           <span className="ml-3">{income} $</span>
         </div>
--- src/Components/TransActionForm/TransActionForm.jsx
+++ src/Components/TransActionForm/TransActionForm.jsx
@@ -1,51 +1,123 @@
 import { useEffect, useRef, useState } from "react";
-import styles from './TransActionForm.module.scss';
+import { IoCheckmarkSharp } from "react-icons/io5";
 
 const TransActionFrom = ({ addTransactions, setIsShow }) => {
-    const [formValues, setFormValues] = useState({
-        desc: '',
-        amount: '',
-        type: 'expense',
+  const [formValues, setFormValues] = useState({
+    desc: "",
+    amount: "",
+    type: "expense",
+  });
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const changeHandler = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
     });
-    const inputRef = useRef();
-
-    useEffect(()=>{
-        inputRef.current.focus();
-    }, [])
-
-    const changeHandler = (e) =>{
-        setFormValues({
-            ...formValues,
-            [e.target.name]: e.target.value,
-        })
-    }
-    const submitHandler = (e) => {
-        e.preventDefault();
-        if(formValues.desc.length && formValues.amount.length){
-        addTransactions(formValues);
-        setFormValues({
-            desc: '',
-            amount: '',
-            type: 'expense'
-        })
-        setIsShow(false);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (formValues.desc.length && formValues.amount.length) {
+      addTransactions(formValues);
+      setFormValues({
+        desc: "",
+        amount: "",
+        type: "expense",
+      });
+      setIsShow(false);
     } else inputRef.current.focus();
-    }
-    return ( 
-        <form onSubmit={submitHandler} className={styles.formContainer}>
-            <fieldset className={styles.inputGroup}>
-                <input ref={inputRef} type="number" name="amount" placeholder="Amount" value={formValues.amount} onChange={changeHandler} />
-                <input type="text" name="desc" placeholder="Description" value={formValues.desc} onChange={changeHandler} />
-            </fieldset>
-            <fieldset className={styles.inputGroup}>
-                <input type="radio" value="expense" checked={formValues.type === "expense"} name="type" id="ExpenseRD" onChange={changeHandler} />
-                <label htmlFor="ExpenseRD">Expense</label>
-                <input type="radio" value="income" checked={formValues.type === "income"} name="type" id="IncomeRD" onChange={changeHandler}/>
-                <label htmlFor="IncomeRD">Income</label>
-            </fieldset>
-            <button type="submit">Transaction</button>
-        </form>
-     );
-}
- 
+  };
+  return (
+    <form onSubmit={submitHandler} className={`flex flex-col py-10 px-5`}>
+      <fieldset className="mb-5">
+        <input
+          ref={inputRef}
+          type="number"
+          name="amount"
+          placeholder="Amount"
+          value={formValues.amount}
+          onChange={changeHandler}
+          className="block w-full mb-5 focus:outline-none border-none rounded-sm px-3 py-2 bg-violet-200 shadow-md shadow-violet-200/50 text-violet-600 placeholder:text-violet-400"
+        />
+        <input
+          type="text"
+          name="desc"
+          placeholder="Description"
+          value={formValues.desc}
+          onChange={changeHandler}
+          className="block w-full focus:outline-none border-none rounded-sm px-3 py-2 bg-violet-200 shadow-md shadow-violet-200/50 text-violet-600 placeholder:text-violet-400"
+        />
+      </fieldset>
+      <fieldset className={`mb-7 flex`}>
+        <label
+          htmlFor="ExpenseRD"
+          className={`flex items-center px-5 py-2 mr-5 rounded-md transition duration-500 cursor-pointer shadow-lg ${
+            formValues.type === "expense"
+              ? "bg-violet-600 text-violet-200 shadow-violet-600/50"
+              : "bg-violet-200 text-violet-600 shadow-violet-200/50"
+          }`}
+        >
+          <input
+            type="radio"
+            value="expense"
+            checked={formValues.type === "expense"}
+            name="type"
+            id="ExpenseRD"
+            onChange={changeHandler}
+            className="sr-only"
+          />
+          <span
+            className={`flex items-center justify-center w-4 h-4 rounded-sm border ${
+              formValues.type === "expense"
+                ? "border-violet-200"
+                : "border-violet-600"
+            } mr-3`}
+          >
+            {formValues.type === "expense" && <IoCheckmarkSharp />}
+          </span>
+          <span>Expense</span>
+        </label>
+        <label
+          htmlFor="IncomeRD"
+          className={`flex items-center px-5 py-2 rounded-md transition duration-500 cursor-pointer shadow-lg ${
+            formValues.type === "income"
+              ? "bg-violet-600 text-violet-200 shadow-violet-600/50"
+              : "bg-violet-200 text-violet-600 shadow-violet-200/50"
+          }`}
+        >
+          <input
+            type="radio"
+            value="income"
+            checked={formValues.type === "income"}
+            name="type"
+            id="IncomeRD"
+            onChange={changeHandler}
+            className="sr-only"
+          />
+          <span
+            className={`flex items-center justify-center w-4 h-4 rounded-sm border ${
+              formValues.type === "income"
+                ? "border-violet-200"
+                : "border-violet-600"
+            }  mr-3`}
+          >
+            {formValues.type === "income" && <IoCheckmarkSharp />}
+          </span>
+          <span>Income</span>
+        </label>
+      </fieldset>
+      <button
+        className={`bg-violet-200 text-violet-600 hover:bg-violet-600 hover:text-violet-200 transition duration-500 py-2 rounded-md`}
+        type="submit"
+      >
+        Transaction
+      </button>
+    </form>
+  );
+};
+
 export default TransActionFrom;
